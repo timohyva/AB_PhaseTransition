77bac834030541e8c61fea831168b8876dd9cf2f
Zhang Kuang <hyvarinentimo00@gmail.com>
2021-09-28 21:35:56 +0300

    use the numpy.interp to implement the SC liner interpolation

--- Module_SC_CorrectionObject_V01.py
+++ Module_SC_CorrectionObject_V01.py
@@ -2,14 +2,17 @@
 # Tc, effective mass, Fermi velocity. The data sheet comes form table. II of
 # PRB 101.024517
 
-# This is version 0.0 (v.0.0) of the intepolation object (class)
+# This is version 0.1 (v.0.1) of the intepolation object (class)
 
 # author: quang. zhang (github@timohyva)
 
 
 # zeta3 = 1.2020569;
 
-# data sheet from talbe.II of PRB. 101. 024517 
+import numpy as np
+
+# data sheet from talbe.II of PRB. 101. 024517
+P = np.arange(0.0, 36.0, 2.0) # pressure date, step 2.0 bar
 c1 = [-0.0098, -0.0127, -0.0155, -0.0181, -0.0207, -0.0231, -0.0254, -0.0275, -0.0295, -0.0314, -0.0330, -0.0345, -0.0358, -0.0370, -0.0381, -0.0391, -0.0402, -0.0413]
 c2 = [-0.0419, -0.0490, -0.0562, -0.0636, -0.0711, -0.0786, -0.0861, -0.0936, -0.1011, -0.1086, -0.1160, -0.1233, -0.1306, -0.1378, -0.1448, -0.1517, -0.1583, -0.1645]
 c3 = [-0.0132, -0.0161, -0.0184, -0.0202, -0.0216, -0.0226, -0.0233, -0.0239, -0.0243, -0.0247, -0.0249, -0.0252, -0.0255, -0.0258, -0.0262, -0.0265, -0.0267, -0.0268]
@@ -18,53 +21,6 @@ c5 = [-0.0899, -0.1277, -0.1602, -0.1880, -0.2119, -0.2324, -0.2503, -0.2660, -0
 Tc = [0.929, 1.181, 1.388, 1.560, 1.705, 1.828, 1.934, 2.026, 2.106, 2.177, 2.239, 2.293, 2.339, 2.378, 2.411, 2.438, 2.463, 2.486]
 Ms = [2.80, 3.05, 3.27, 3.48, 3.68, 3.86, 4.03, 4.20, 4.37, 4.53, 4.70, 4.86, 5.02, 5.18, 5.34, 5.50, 5.66, 5.82] # in unit of helium-3 atom
 VF = [59.03, 55.41, 52.36, 49.77, 47.56, 45.66, 44.00, 42.51, 41.17, 39.92, 38.74, 37.61, 36.53, 35.50, 34.53, 33.63, 32.85, 32.23]
-
-
-def region_judgement(p):
-    if (p >= 0) and (p < 2):
-         pk=0;pk1=2;k=0
-    if (p >= 2) and (p < 4):
-         pk=2;pk1=4;k=1
-    if (p >= 4) and (p < 6):
-         pk=4;pk1=6;k=2
-    if (p >= 6) and (p < 8):
-         pk=6;pk1=8;k=3
-    if (p >= 8) and (p < 10):
-         pk=8;pk1=10;k=4
-    if (p >= 10) and (p < 12):
-         pk=10;pk1=12;k=5
-    if (p >= 12) and (p < 14):
-         pk=12;pk1=14;k=6
-    if (p >= 14) and (p < 16):
-         pk=14;pk1=16;k=7
-    if (p >= 16) and (p < 18):
-         pk=16;pk1=18;k=8     
-    if (p >= 18) and (p < 20):
-         pk=18;pk1=20;k=9
-    if (p >= 20) and (p < 22):
-         pk=20;pk1=22;k=10
-    if (p >= 22) and (p < 24):
-         pk=22;pk1=24;k=11
-    if (p >= 24) and (p < 26):
-         pk=24;pk1=26;k=12
-    if (p >= 26) and (p < 28):
-         pk=26;pk1=28;k=13
-    if (p >= 28) and (p < 30):
-         pk=28;pk1=30;k=14     
-    if (p >= 30) and (p < 32):
-         pk=30;pk1=32;k=15
-    if (p >= 32) and (p < 34):
-         pk=32;pk1=34;k=16
-
-    return [pk, pk1, k] #return list
-
-
-def intepolation_presure_SC(pk,pk1,p,fk,fk1):
-    K = (fk1-fk)/(pk1-pk);print("K =",K, "pk1 =",pk1, "pk =",pk, "fk1 =",fk1, "fk =",fk)
-    fip = K * (p-pk) + fk;print("fip =",fip)
-
-    # return the cip or Tcp
-    return fip
          
 class BETA:
 
@@ -75,29 +31,31 @@ class BETA:
          self.name = name
          print(" beta oject is crated ")
 
-     def c1_function(self,c1,pressure,pk,pk1,k):
-         self.c1p =  intepolation_presure_SC(pk,pk1,pressure,c1[k],c1[k+1])
+     def c1_function(self,P,c1,pressure):
+         print(pressure)
+         self.c1p =  np.interp(pressure,P,c1)
+         print(self.c1p)
 
-     def c2_function(self,c2,pressure,pk,pk1,k):
-         self.c2p =  intepolation_presure_SC(pk,pk1,pressure,c2[k],c2[k+1])
+     def c2_function(self,P,c2,pressure):
+         self.c2p =  np.interp(pressure,P,c2)
 
-     def c3_function(self,c3,pressure,pk,pk1,k):
-         self.c3p =  intepolation_presure_SC(pk,pk1,pressure,c3[k],c3[k+1])
+     def c3_function(self,P,c3,pressure):
+         self.c3p =  np.interp(pressure,P,c3)
 
-     def c4_function(self,c4,pressure,pk,pk1,k):
-         self.c4p =  intepolation_presure_SC(pk,pk1,pressure,c4[k],c4[k+1])
+     def c4_function(self,P,c4,pressure):
+         self.c4p =  np.interp(pressure,P,c4)
 
-     def c5_function(self,c5,pressure,pk,pk1,k):
-         self.c5p =  intepolation_presure_SC(pk,pk1,pressure,c5[k],c5[k+1])
+     def c5_function(self,P,c5,pressure):
+         self.c5p =  np.interp(pressure,P,c5)
 
-     def tc_function(self,Tc,pressure,pk,pk1,k):
-         self.tcp =  intepolation_presure_SC(pk,pk1,pressure,Tc[k],Tc[k+1])
+     def tc_function(self,P,Tc,pressure):
+         self.tcp =  np.interp(pressure,P,Tc)
 
-     def mStar_function(self,MS,pressure,pk,pk1,k):
-         self.ms =  intepolation_presure_SC(pk,pk1,pressure,Ms[k],Ms[k+1])
+     def mStar_function(self,P,MS,pressure):
+         self.ms =  np.interp(pressure,P,MS)
 
-     def vFermi_function(self,VF,pressure,pk,pk1,k):
-         self.vf = intepolation_presure_SC(pk,pk1,pressure,VF[k],VF[k+1])
+     def vFermi_function(self,P,VF,pressure):
+         self.vf = np.interp(pressure,P,VF)
          
          
 
--- test_SC_module_V01.py
+++ test_SC_module_V01.py
@@ -1,39 +1,58 @@
-# This scipt is for testing the SC correction module
+# This scipt is for testing the SC correction module v.0.1
 
-# import matplotlib.pyplot as plot1
+import matplotlib.pyplot as plot1
 import numpy as np
 # from math import pi
 
-
-import Module_SC_CorrectionObject as SC # strong coupling correction module
+import Module_SC_CorrectionObject_V01 as SC # strong coupling correction module
 
 # test the object
 
 BetaObject = SC.BETA('betaAndTc') # us class BETA in mudule SC to create BetaObject
 
 # p = 32 # pressure, bar
-pressureArray = np.arange(0.0, 34.0, 2.0)
+pressureArray = np.arange(0.0, 34.0+2.0, 2.0)
 
 for p in pressureArray:
     
-    judgementList = SC.region_judgement(p) # region_judgement is in SC module
+    BetaObject.c1_function(SC.P,SC.c1,p);c1p = BetaObject.c1p
+    BetaObject.c2_function(SC.P,SC.c2,p);c2p = BetaObject.c2p
+    BetaObject.c3_function(SC.P,SC.c3,p);c3p = BetaObject.c3p
+    BetaObject.c4_function(SC.P,SC.c4,p);c4p = BetaObject.c4p
+    BetaObject.c5_function(SC.P,SC.c5,p);c5p = BetaObject.c5p
+    BetaObject.tc_function(SC.P,SC.Tc,p);Tcp = BetaObject.tcp
 
-    print('judgementList is', judgementList)
-
-    print('low pressure is: ',judgementList[0], ',high pressure is: ',judgementList[1], ',interpolation region is: ', judgementList[2])
-
-    pk = judgementList[0]; pk1 = judgementList[1]; k = judgementList[2]
-
-    BetaObject.c1_function(SC.c1,p,pk,pk1,k);c1p = BetaObject.c1p
-    BetaObject.c2_function(SC.c2,p,pk,pk1,k);c2p = BetaObject.c2p
-    BetaObject.c3_function(SC.c3,p,pk,pk1,k);c3p = BetaObject.c3p
-    BetaObject.c4_function(SC.c4,p,pk,pk1,k);c4p = BetaObject.c4p
-    BetaObject.c5_function(SC.c5,p,pk,pk1,k);c5p = BetaObject.c5p
-    BetaObject.tc_function(SC.Tc,p,pk,pk1,k);Tcp = BetaObject.tcp
-
-    BetaObject.mStar_function(SC.Ms,p,pk,pk1,k);mEffective = BetaObject.ms
-    BetaObject.vFermi_function(SC.VF,p,pk,pk1,k);vFermi = BetaObject.vf
+    BetaObject.mStar_function(SC.P,SC.Ms,p);mEffective = BetaObject.ms
+    BetaObject.vFermi_function(SC.P,SC.VF,p);vFermi = BetaObject.vf
 
     print('\npressure is ',p,' ,c1p is ',c1p,' ,c2p is ',c2p,' ,c3p is ',c3p,' ,c4p is ',c4p,' ,c4p ',' ,c5p ',c5p,' ,tcp is ',Tcp,' ,effective mass is ', mEffective,' ,Fermi velocity is ', vFermi,'\n\n\n')
 
     print('\npressure is ',p,' ,effective mass is ', mEffective,' ,Fermi velocity is ', vFermi,'\n\n\n')
+
+
+# test the vectorization, SC_Container is container list for all coefficents arraies,
+
+SC_Container = []
+BetaObject.c1_function(SC.P,SC.c1,pressureArray);SC_Container.append(BetaObject.c1p)
+BetaObject.c2_function(SC.P,SC.c2,pressureArray);SC_Container.append(BetaObject.c2p)
+BetaObject.c3_function(SC.P,SC.c3,pressureArray);SC_Container.append(BetaObject.c3p)
+BetaObject.c4_function(SC.P,SC.c4,pressureArray);SC_Container.append(BetaObject.c4p)
+BetaObject.c5_function(SC.P,SC.c5,pressureArray);SC_Container.append(BetaObject.c5p)
+BetaObject.tc_function(SC.P,SC.Tc,pressureArray);SC_Container.append(BetaObject.tcp)
+
+BetaObject.mStar_function(SC.P,SC.Ms,pressureArray);SC_Container.append(BetaObject.ms)
+BetaObject.vFermi_function(SC.P,SC.VF,pressureArray);SC_Container.append(BetaObject.vf)
+
+print(SC_Container)
+
+plot1.plot(pressureArray,SC_Container[0],'-x',pressureArray,SC.c1,'or')
+plot1.plot(pressureArray,SC_Container[1],'-x',pressureArray,SC.c2,'or')
+plot1.plot(pressureArray,SC_Container[2],'-x',pressureArray,SC.c3,'or')
+plot1.plot(pressureArray,SC_Container[3],'-x',pressureArray,SC.c4,'or')
+plot1.plot(pressureArray,SC_Container[4],'-x',pressureArray,SC.c5,'or')
+plot1.plot(pressureArray,SC_Container[5],'-x',pressureArray,SC.Tc,'or')
+plot1.plot(pressureArray,SC_Container[6],'-x',pressureArray,SC.Ms,'or')
+plot1.plot(pressureArray,SC_Container[7],'-x',pressureArray,SC.VF,'or')
+
+plot1.show()
+
